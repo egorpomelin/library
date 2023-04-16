64c97a6c73ad06caca6efc895b61da9d6d50f006
egorp <egorpomelin@gmail.com>
2023-04-16 22:01:28 +0300

    Добавлины модели автор, категория, жанр

--- library_main/models.py
+++ library_main/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    image = models.ImageField(verbose_name='Изображение')
+    name = models.CharField(verbose_name='ФИО', max_length=50)
+
+class Category(models.Model):
+    name = models.CharField(verbose_name='Категория')
+
+class Genre(models.Model):
+    name = models.CharField(verbose_name='Название жанра')
+    category = models.ForeignKey(Category, verbose_name='Категория жанра', on_delete=models.CASCADE)
+
+class Book(models.Model):
+
+    name = models.CharField(verbose_name='Название книги', max_length=50)
+    author = models.ForeignKey(Author, verbose_name='Автор книги', on_delete=models.CASCADE)
+    genre = models.ForeignKey(Genre, verbose_name='Жанр книги', on_delete=models.CASCADE)
