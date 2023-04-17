72912640b9fd6aa876592449a656eae8d8cf9184
egorp <egorpomelin@gmail.com>
2023-04-17 22:06:33 +0300

    lj,fdktybt vjltkb rybub

--- library_main/models.py
+++ library_main/models.py
@@ -4,15 +4,44 @@ class Author(models.Model):
     image = models.ImageField(verbose_name='Изображение')
     name = models.CharField(verbose_name='ФИО', max_length=50)
 
+    class Meta:
+        verbose_name = "Автор"
+        verbose_name_plural = "Авторы"
+
+    def __str__(self):
+        return self.name
+
 class Category(models.Model):
     name = models.CharField(verbose_name='Категория')
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+    def __str__(self):
+        return self.name
 
 class Genre(models.Model):
     name = models.CharField(verbose_name='Название жанра')
     category = models.ForeignKey(Category, verbose_name='Категория жанра', on_delete=models.CASCADE)
 
-class Book(models.Model):
+    class Meta:
+        verbose_name = "Жанр"
+        verbose_name_plural = "Жанры"
 
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
     name = models.CharField(verbose_name='Название книги', max_length=50)
     author = models.ForeignKey(Author, verbose_name='Автор книги', on_delete=models.CASCADE)
     genre = models.ForeignKey(Genre, verbose_name='Жанр книги', on_delete=models.CASCADE)
+    description = models.TextField(verbose_name='Описание книги')
+    image = models.ImageField(verbose_name='Обложка книги')
+
+    class Meta:
+        verbose_name = "Книга"
+        verbose_name_plural = "Книги"
+
+    def __str__(self):
+        return self.name
