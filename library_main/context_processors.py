5816d35bc307f57a993362a1eb1ca8859e9f6bf3
egorp <egorpomelin@gmail.com>
2023-04-23 22:09:03 +0300

    главная

--- library_main/context_processors.py
+++ library_main/context_processors.py
@@ -4,5 +4,5 @@ from library_main.models import Category, Genre, News
 def category(request):
     category = Category.objects.all().order_by('name')
     genre = Genre.objects.all()
-    news = News.objects.all().order_by('-pudlicachia_date')
+    news = News.objects.all().order_by('-pudlicachia_date')[:6]
     return {'category' : category, 'genre' : genre, 'news' : news}
--- library_main/views.py
+++ library_main/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from library_main.models import Author, Category, Genre, Book, News, Quotes
 
 def index(request):
-
-    #context = {'news' : news}
-    return render(request, 'index.html',) #context=context
+    now = Book.objects.all().order_by('-date_of_writing')[:3]
+    context = {'now' : now,}
+    return render(request, 'index.html', context=context)
